dbad9bc7d11e2c7aa6f03690620fcded59d1158d
AdamSwenson <adam.swenson@csun.edu>
2020-10-21 16:07:29 -0700

    initial commit

---
+++ app/Http/Controllers/MotionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Motion;
+use Illuminate\Http\Request;
+
+class MotionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Motion  $motion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Motion $motion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Motion  $motion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Motion $motion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Motion  $motion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Motion $motion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Motion  $motion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Motion $motion)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_10_21_225335_create_motions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('motions', function (Blueprint $table) {
+            /** The thing being voted upon */
+            $table->text('content');
+
+            /** Helper text to display */
+            $table->text('description')->nullable();
+
+            /** Majority, 2/3, etc */
+            $table->text('requires');
+
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('motions');
+    }
+}
