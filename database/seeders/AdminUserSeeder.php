0d6ae57181be97a2ba9278f9ff6f696c56567692
AdamSwenson <adam.swenson@csun.edu>
2020-11-11 17:27:03 -0800

    added admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try{
+
+            $props = [
+                'first_name' => 'Admin',
+                'last_name' => 'User',
+                'email' => env('DEV_ADMIN_EMAIL'),
+                'password' => env('DEV_ADMIN_PASS')
+                ];
+
+            User::create($props);
+
+            
+        }catch(Exception $e){
+            Log::error($e);
+        }
+        //
+    }
+}
