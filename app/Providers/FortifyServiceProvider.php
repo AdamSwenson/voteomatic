349a86eff25a5f3c27519132eabe6a67b17dc296
AdamSwenson <adam.swenson@csun.edu>
2021-04-01 14:26:43 -0700

    fixed problem with fortify not being able to find the login page view by updating the fortifyservice provider. this was required after updating version of fortify.

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -28,6 +28,7 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Fortify::loginView(fn () => view('auth.login'));
         Fortify::createUsersUsing(CreateNewUser::class);
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
